3e052d5468734ac35ae5c4eb11068e6c5550b76e
Harry <hjwp2@cantab.net>
2011-11-26 11:09:04 +0000

    integrating django-articles blog. add database, blog urls, move base.html

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -107,6 +107,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'tddjango_site.urls'
 
 TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- urls.py
+++ urls.py
@@ -1,21 +1,18 @@
 ﻿from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import include, patterns, url
 
 from tddjango_site.tutorials import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.tutorial, name='home'),
     url(r'^tutorial/(\d+)/$', views.tutorial, name='tutorial'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('articles.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 # static files via django - not ideal
 urlpatterns += staticfiles_urlpatterns()
