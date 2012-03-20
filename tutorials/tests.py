6aceca5a18e4a8f0f6acd8bc9e822b7f440750b7
Harry <hjwp2@cantab.net>
2012-03-20 14:36:28 +0000

    Fix unit test for rst directive

--- tutorials/tests.py
+++ tutorials/tests.py
@@ -9,7 +9,7 @@ from tddjango_site.tutorials.views import tutorial
 
 class RSTDirectiveTest(TestCase):
 
-    @patch('tutorials.views.publish_parts')
+    @patch('tddjango_site.tutorials.views.publish_parts')
     def test_tutorial_view_can_handle_filename_sourcecode_rst_directive(self, mock_publish):
         some_rst = dedent('''
             normal string
@@ -33,8 +33,3 @@ class RSTDirectiveTest(TestCase):
         self.assertIn('<pre>', response.content)
         self.assertIn('<p class="caption">path/to/filename.py</p>', response.content)
 
-
-        body_html = publish_parts(some_rst, writer_name='html')['html_body']
-        self.assertIn('<h1>header</h1>', body_html)
-        self.assertIn('<pre>', body_html)
-        self.assertIn('<p class="caption">path/to/filename.py</p>', body_html)
