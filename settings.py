ed0944a3ac83a7ea7bedca147585559c03d7e8de
Harry <hjwp2@cantab.net>
2013-01-28 22:23:10 +0000

    Switch debug to false and start serving static files properly

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/static/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
