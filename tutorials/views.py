617283368a6f61934b2cb1c9ceb067a56d8530eb
Harry <hjwp2@cantab.net>
2013-04-15 15:04:01 +0000

    part 6 redirects to testing goat, put link to book in nav.

--- tutorials/views.py
+++ tutorials/views.py
@@ -2,12 +2,13 @@ from docutils.core import publish_parts
 import os
 
 from docutils.parsers.rst import directives
+from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 from tddjango_site.tutorials.rst_directive import PygmentsWithFilenameCaption
 
 ROOT = os.path.split(os.path.dirname(__file__))[0]
-PAGE_NUMBERS = [1, 2, 3, 4, 5, 6]
+PAGE_NUMBERS = [1, 2, 3, 4, 5]
 
 def tutorial(request, number=None):
     directives.register_directive('sourcecode', PygmentsWithFilenameCaption)
@@ -15,6 +16,8 @@ def tutorial(request, number=None):
         rst_file = os.path.join(ROOT, 'source', 'README.rst')
     else:
         number = int(number)
+        if number == 6:
+            return HttpResponsePermanentRedirect('http://www.obeythetestinggoat.com/writing-a-book-preview.html')
         rst_file = os.path.join(ROOT, 'source', 'tutorial%02d.rst' % number)
     with open(rst_file) as f:
         html_parts = publish_parts(f.read(), writer_name='html')
